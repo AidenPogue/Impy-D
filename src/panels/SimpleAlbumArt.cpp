91fb611ee1a7edeef6b6cf9e1d715563ddd4be58
Aiden Pogue <dudarino3000@gmail.com>
2026-02-10 21:58:09 -0800

    feat: Update simple album art to work with new async methods

--- src/panels/SimpleAlbumArt.cpp
+++ src/panels/SimpleAlbumArt.cpp
@@ -1,25 +1,46 @@
 #include "SimpleAlbumArt.hpp"
 
+#include <cmath>
+#include <iostream>
+
 #include "imgui.h"
 #include "../Utils.hpp"
 #include "../Mpd/AlbumArtHelper.hpp"
 
-void ImMPD::SimpleAlbumArt::SetArtwork(MpdClientWrapper &client, const std::string &uri)
+void ImMPD::SimpleAlbumArt::SetArtwork(const ImpyD::Mpd::AlbumArtHelper::Result &img)
 {
-    if (currentSongUri == uri)
-    {
-        return;
-    }
-
     if (currentArtTexture)
     {
         glDeleteTextures(1, &currentArtTexture);
     }
 
-    ImVec2 dimensions;
-    currentArtTexture = ImpyD::Mpd::AlbumArtHelper::LoadArtworkToGPU(client, uri, dimensions, false);
-    currentArtAspect = dimensions.x / dimensions.y;
-    currentSongUri = uri;
+    std::cout << "Uploading" << std::endl;
+    ImpyD::Utils::UploadTexture(img.data.get(), img.width, img.height, &currentArtTexture);
+
+    currentArtAspect = (float)img.width / img.height;
+}
+
+void ImMPD::SimpleAlbumArt::CheckFutures(MpdClientWrapper &client)
+{
+    if (ImpyD::Utils::IsReady(artFuture))
+    {
+        SetArtwork(artFuture.get());
+    }
+    else if (ImpyD::Utils::IsReady(songFuture))
+    {
+        auto s = songFuture.get();
+        if (!s)
+        {
+            return;
+        }
+        auto newUri = s->GetUri();
+        if (newUri == currentSongUri)
+        {
+            return;
+        }
+        currentSongUri = newUri;
+        artFuture = ImpyD::Mpd::AlbumArtHelper::LoadArtworkAsync(client, currentSongUri, false);
+    }
 }
 
 std::string ImMPD::SimpleAlbumArt::PanelName()
@@ -29,6 +50,8 @@ std::string ImMPD::SimpleAlbumArt::PanelName()
 
 void ImMPD::SimpleAlbumArt::DrawContents(MpdClientWrapper &client)
 {
+    CheckFutures(client);
+
     //Can we do this?
     if (currentArtTexture)
     {
@@ -52,41 +75,50 @@ void ImMPD::SimpleAlbumArt::DrawContents(MpdClientWrapper &client)
             auto cursor = ImGui::GetCursorPos();
             cursor.x += available.x / 2 - size.x / 2;
             cursor.y += available.y / 2 - size.y / 2;
+
+            cursor.x = std::round(cursor.x);
+            cursor.y = std::round(cursor.y);
+            size.x = std::round(size.x);
+            size.y = std::round(size.y);
+
             ImGui::SetCursorPos(cursor);
         }
 
-        //Double cast baybee
+        //const float loadingBrightness = .5f;
+        //auto tint = artFuture.valid() ? ImVec4(loadingBrightness, loadingBrightness, loadingBrightness , 1) : ImVec4(1, 1, 1, 1);
+        //ImGui::ImageWithBg((ImTextureID)(intptr_t)currentArtTexture, size, ImVec2(0,0), ImVec2(1,1), ImVec4(), tint);
         ImGui::Image((ImTextureID)(intptr_t)currentArtTexture, size);
     }
 }
 
-void ImMPD::SimpleAlbumArt::SetCurrentArtwork(MpdClientWrapper &client)
+void ImMPD::SimpleAlbumArt::RequestCurrentArtwork(MpdClientWrapper &client)
 {
-    auto &curSong = client.GetCurrentSong();
-    if (curSong)
-    {
-        SetArtwork(client, mpd_song_get_uri(curSong.get()));
-    }
-    else
-    {
-        currentArtTexture = 0;
-    }
+    songFuture = client.GetCurrentSong();
+    artFuture = {};
 }
 
 void ImMPD::SimpleAlbumArt::OnIdleEvent(MpdClientWrapper &client, mpd_idle event)
 {
     if (event & MPD_IDLE_PLAYER)
     {
-        SetCurrentArtwork(client);
+        RequestCurrentArtwork(client);
     }
 }
 
 void ImMPD::SimpleAlbumArt::InitState(MpdClientWrapper &client)
 {
-    SetCurrentArtwork(client);
+    RequestCurrentArtwork(client);
 }
 
 void ImMPD::SimpleAlbumArt::DrawContextMenu(MpdClientWrapper &client)
 {
     ImGui::MenuItem("Preserve Aspect Ratio", nullptr, &preserveAspectRatio);
+    if (ImGui::BeginMenu("Load Artwork When..."))
+    {
+        ImGui::MenuItem("Song Changes", nullptr);
+        ImGui::MenuItem("Album + Album Artist Changes", nullptr);
+
+        ImGui::EndMenu();
+    }
+
 }
