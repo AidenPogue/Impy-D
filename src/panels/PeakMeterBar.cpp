9e9a420ddfb6120f3ac1b95a179a69c3c7f1aec8
Aiden Pogue <dudarino3000@gmail.com>
2026-05-03 10:41:42 -0700

    feat: Add PeakMeterBar
    Class that draws a 0-1 filled bar with a falling peak indicator. Object keeps track of peak value and styling

---
+++ src/panels/PeakMeterBar.cpp
@@ -0,0 +1,76 @@
+#include "PeakMeterBar.hpp"
+
+
+namespace ImpyD
+{
+
+    const PeakMeterBar::Style PeakMeterBar::defaultStyle;
+
+    float LerpMainAxisValue(float min, float max, float value, bool reverse)
+    {
+        return std::lerp(min, max, reverse ? 1 - value : value);
+    }
+
+    void PeakMeterBar::Draw(Context &context, ImVec2 min, ImVec2 max, float value, bool vertical)
+    {
+        auto useStyle = style ? style : &defaultStyle;
+
+        peakTime += context.GetDeltaTime();
+
+        if (peakTime > useStyle->peakFallDelay)
+        {
+            isFalling = true;
+        }
+
+        if (isFalling)
+        {
+            peak -= useStyle->peakFallSpeed * context.GetDeltaTime();
+        }
+
+        if (value > peak)
+        {
+            isFalling = false;
+            peak = value;
+            peakTime = 0;
+        }
+
+        auto drawList = ImGui::GetWindowDrawList();
+        auto round = useStyle->rounding;
+        drawList->AddRectFilled(min, max, useStyle->backgroundColor, round);
+
+        auto fillMax = max, fillMin = min;
+
+        if (!useStyle->reverse)
+        {
+            if (vertical)
+            {
+                fillMax.y = std::lerp(min.y, max.y, value);
+            }
+            else
+            {
+                fillMax.x = std::lerp(min.x, max.x, value);
+            }
+        }
+        else
+        {
+            if (vertical)
+            {
+                fillMin.y = std::lerp(min.y, max.y, 1 - value);
+            }
+            else
+            {
+                fillMin.x = std::lerp(min.x, max.x, 1 - value);
+            }
+
+        }
+
+        drawList->AddRectFilled(fillMin, fillMax, useStyle->foregroundColor, round);
+
+        auto peakMin = min;
+        peakMin.x = std::lerp(min.x, max.x, peak);
+        auto peakMax = peakMin;
+        peakMax.x += useStyle->peakWidth;
+        peakMax.y = max.y;
+        drawList->AddRectFilled(peakMin, peakMax, useStyle->peakColor);
+    }
+} // ImpyD
---
+++ src/panels/PeakMeterBar.hpp
@@ -0,0 +1,38 @@
+#ifndef IM_MPD_PEAKMETERBAR_HPP
+#define IM_MPD_PEAKMETERBAR_HPP
+#include "imgui.h"
+#include "../Context.hpp"
+
+namespace ImpyD
+{
+
+    // Draws a filled bar based on a 0-1 percentage with a falling peak.
+    class PeakMeterBar
+    {
+    public:
+        struct Style
+        {
+            ImU32 backgroundColor = IM_COL32(50, 50, 50, 255), foregroundColor = IM_COL32(0, 255, 0, 255), peakColor = IM_COL32(255, 0, 0, 255);
+            float rounding = 0;
+
+            float peakFallSpeed = .125f, peakFallDelay = .35f;
+            float peakWidth = 1;
+            bool showPeak = true;
+
+            bool reverse = false;
+        };
+
+        static const Style defaultStyle;
+
+    private:
+        float peak = -1;
+        bool isFalling = true;
+        double peakTime = 0;
+
+    public:
+        void Draw(Context &context, ImVec2 min, ImVec2 max, float value, bool vertical);
+        const Style *style = nullptr;
+    };
+} // ImpyD
+
+#endif //IM_MPD_PEAKMETERBAR_HPP
