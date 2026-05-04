72c2c37432ff38bd258eda3d5924188cb6500e1c
Aiden Pogue <dudarino3000@gmail.com>
2026-05-03 17:50:22 -0700

    fix: Make VolumeMeter display dBFS instead of raw PCM values
    Also add simple smoothing to values passed to the bars, but this will be moved to the bar itself.

--- src/panels/VolumeMeter.cpp
+++ src/panels/VolumeMeter.cpp
@@ -8,21 +8,25 @@ std::string ImpyD::VolumeMeter::PanelName()
 
 void ImpyD::VolumeMeter::DrawContents(Context &context)
 {
-    auto sampleWindow = 100;
+    auto sampleWindow = 200;
 
-    left = right = 0;
+    int lTarget = 0, rTarget = 0;
+    //left = right = 0;
     for (int i = 0; i < sampleWindow * 2; i += 2)
     {
         auto idx = context.GetFifoReader().GetFrontIndex() - i;
 
-        left  += context.GetFifoReader().buffer[idx % context.GetFifoReader().buffer.size()];
-        right += context.GetFifoReader().buffer[(idx - 1)  % context.GetFifoReader().buffer.size()];
+        lTarget = std::max(std::abs(context.GetFifoReader().buffer[idx % context.GetFifoReader().buffer.size()]), lTarget);
+        rTarget = std::max(std::abs(context.GetFifoReader().buffer[(idx - 1) % context.GetFifoReader().buffer.size()]), rTarget);
     }
 
-    left /= sampleWindow;
-    right /= sampleWindow;
+    left = std::lerp(left, lTarget, .25f);
+    right = std::lerp(right, rTarget, .25f);
 
-    auto percentL = std::pow(std::abs(left / 32768.0f), .5f), percentR = std::pow(std::abs(right / 32768.0f), .5f);
+    auto lDBFS = 20 * std::log10(left / 32768.0f), rDBFS = 20 * std::log10(right / 32768.0f);
+
+    const float minDBFS = -60;
+    auto percentL = (lDBFS - minDBFS) / (-minDBFS), percentR = (rDBFS - minDBFS) / (-minDBFS);
 
     auto tl1 = ImGui::GetCursorScreenPos();
     auto br1 = ImVec2(tl1.x + ImGui::GetContentRegionAvail().x, tl1.y + ImGui::GetContentRegionAvail().y / 2);
